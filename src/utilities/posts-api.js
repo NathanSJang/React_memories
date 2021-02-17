7736bf98efdabbeed959ad8b88edc00263fae5ef
Jang <sbtan1989@gmail.com>
2021-02-17 13:15:47 -0800

    deploy

--- src/utilities/posts-api.js
+++ src/utilities/posts-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:3001' });
+const API = axios.create({ baseURL: 'https://memories-nj.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
